f48d882ececaf1a80d9e6efbb8086b0b906cf446
Alexander <papaburgerr@gmail.com>
2023-08-24 21:08:28 +1000

    домашка

---
+++ src/mock/point.js
@@ -0,0 +1,30 @@
+const MOCK_POINTS = [
+  {
+    id: 'mockId1',
+    basePrice: 1100,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    dateTo: '2019-07-11T11:22:13.375Z',
+    isFavorite: false,
+    destination: 'destinationId1',
+    offers: [
+      'offersId1'
+    ],
+    type: 'taxi'
+  },
+  {
+    id: 'mockId2',
+    basePrice: 200,
+    dateFrom: '2019-07-10T22:55:56.845Z',
+    dateTo: '2019-07-11T11:22:13.375Z',
+    isFavorite: true,
+    destination: 'destinationId2',
+    offers: [
+      'offersId2'
+    ],
+    type: 'bus'
+  },
+];
+
+export{
+  MOCK_POINTS
+};
